2f90cbf4b15ae5f375123a99f536df2543928fc9
Daniel Hunter <officialdanielhunter@gmail.com>
2017-09-07 17:38:34 -0700

    Major refactor, added CSS file

---
+++ trello_big_edit/js/trello_big_edit.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name           Full screen card view for Trello
+// @author         Justin Kelly http://justin.kelly.org.au @_justin_kelly
+// @version        2
+// @include        https://trello.com/*
+// @include        http://trello.com/*
+// ==/UserScript==
+
+// cssString = ' '+
+// 	'.window{ '+
+// 	'	left:0px !important; ' +
+// 	'	top:0px !important; ' +
+// 	'	width:90% !important; '+
+// 	'} '+
+// 	' '+
+// 	'.window-main-col '+
+// 	'{ '+
+// 	'	width:80% !important; '+
+// 	'} '+
+// 	'.window-main-col .edit textarea.field '+
+// 	'{ '+
+// 	'	min-height:492px; '+
+// 	'} '+
+// 	'.window-wrapper{ '+
+// 	'	height: 100%; '+
+// 	'} ';
+//
+// insertCSS(cssString);
+// // Function to insert CSS
+// function insertCSS(cssToInsert) {
+// 	var head=document.getElementsByTagName('head')[0];
+// 	if(!head)
+// 		return;
+// 	var style=document.createElement('style');
+// 	style.setAttribute('type','text/css');
+// 	style.appendChild(document.createTextNode(cssToInsert));
+// 	head.appendChild(style);
+// }
